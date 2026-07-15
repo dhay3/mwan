43d6857350e6d13d0814bd6ce319aaf05d72f214
cyberpelican <62749885+dhay3@users.noreply.github.com>
2026-07-15 16:57:38 +0800

    🚧 chore: wip

--- src/probe/__init__.py
+++ src/probe/__init__.py
@@ -5,11 +5,11 @@ from config import MwanConfig
 from . import ICMP, TCP
 
 
-logger = logging.getLogger("Probe")
+logger = logging.getLogger('Probe')
 
 
 def ping(config: MwanConfig, addr: str) -> bool:
-    if ":" in addr:
+    if ':' in addr:
         return TCP.ping(config, addr)
     return ICMP.ping(config, addr)
 
@@ -21,9 +21,12 @@ def probe(config: MwanConfig, enable_log: bool = True) -> bool:
         try:
             puls = ping(config, addr)
         except Exception as exc:
-            raise RuntimeError(f"trans:{uid} addr:{addr} failed") from exc
-        if enable_log and not puls:
-            logger.debug(f"trans:{uid} addr:{addr} timeouted")
+            raise RuntimeError(f'trans:{uid} addr:{addr} failed') from exc
+        if enable_log:
+            if puls:
+                logger.debug(f'trans:{uid} addr:{addr} succeeded')
+            else:
+                logger.debug(f'trans:{uid} addr:{addr} timeouted')
         pulses.append(puls)
 
     down = not any(pulses)
